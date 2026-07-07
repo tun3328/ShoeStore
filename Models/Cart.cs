95d59ba6c19edbad4a40c70ad2972d59b6b9914e
tun3328 <2400006570@nttu.edu.vn>
2026-07-07 09:38:56 +0700

    Add files via upload

---
+++ Models/Cart.cs
@@ -0,0 +1,21 @@
+﻿using EshopperMCV.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EShopperMCV.Models
+{
+    public class Cart
+    {
+        public Product Product { get; set; }
+        public int Quantity { get; set; }
+
+        // Constructor
+        public Cart(Product product, int quantity)
+        {
+            Product = product;
+            Quantity = quantity;
+        }
+    }
+}
---
+++ Models/CommentViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EshopperMCV.Models
+{
+    public class CommentViewModel
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Message { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+
+}
