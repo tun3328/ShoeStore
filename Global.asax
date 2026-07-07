becd1a66f3e746e73a44f28cbf57abe0df28b0e7
tun3328 <2400006570@nttu.edu.vn>
2026-07-07 09:32:53 +0700

    Add files via upload

---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="EshopperMCV.MvcApplication" Language="C#" %>
---
+++ Global.asax.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+using System.Web.Security;
+
+namespace EshopperMCV
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+        }
+        protected void Application_AuthenticateRequest(object sender, EventArgs e)
+        {
+            HttpCookie authCookie = Request.Cookies[FormsAuthentication.FormsCookieName];
+            if (authCookie == null) return;
+
+            FormsAuthenticationTicket ticket =
+                FormsAuthentication.Decrypt(authCookie.Value);
+
+            if (ticket == null) return;
+
+            string[] roles = ticket.UserData.Split(',');
+
+            HttpContext.Current.User = new GenericPrincipal(
+                new FormsIdentity(ticket),
+                roles
+            );
+        }
+
+    }
+}
